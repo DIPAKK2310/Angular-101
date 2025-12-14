caa8d38000204215e743d044d2f6d5eb3fb1f5d3
DIPAKK2310 <dipakkhare217@gmail.com>
2025-12-14 09:40:05 +0000

    feat:pages

---
+++ src/app/pages/produts/produts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produts',
+  imports: [],
+  templateUrl: './produts.html',
+  styleUrl: './produts.css',
+})
+export class Produts {
+
+}
