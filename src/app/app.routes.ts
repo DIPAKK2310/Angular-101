74beb017e322e74e215a10c57ddc609b73cbf8b5
DIPAKK2310 <dipakkhare217@gmail.com>
2025-12-15 12:06:50 +0000

    feat:routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { AboutComponent } from './pages/about/about';
+import { ProductsComponent } from './pages/products/products';
+import { ContactComponent } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        title: 'Home Page',
+    },
+    {
+        path:'about',
+        component: AboutComponent,
+        title: 'About Page',
+
+    },
+    {
+        path:'products',
+        component: ProductsComponent,
+        title: 'Products Page',
+    },
+    {
+        path:'contact',
+        component:ContactComponent,
+        title: 'Contact Page',
+    }
+];
